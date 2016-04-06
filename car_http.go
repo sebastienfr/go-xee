457b2a57b13e01979323890f14101ba7bb568531
Guillaume Bailleul <gbailleul@eliocity.com>
2016-04-06 23:25:32 +0200

    Added find car by ID

--- car_http.go
+++ car_http.go
@@ -6,6 +6,7 @@ import (
 
 const (
     carsURL = "/v3/users/%d/cars"
+    carURL  = "/v3/users/%d/cars/%d"
 )
 
 // FindCars for a given user
@@ -17,4 +18,15 @@ func (s *SDK) FindCars(userID int64, token string) ([]Car, error) {
     err := s.decodeGetRequest(uri, token, params, &cars)
 
     return cars, err
+}
+
+// FindCarByID find with ID for a given user
+func (s *SDK) FindCarByID(userID int64, carID int64, token string) (Car, error) {
+    var car = Car{}
+    var params = make(map[string]string)
+
+    uri := fmt.Sprintf(carURL, userID, carID)
+    err := s.decodeGetRequest(uri, token, params, &car)
+
+    return car, err
 }
