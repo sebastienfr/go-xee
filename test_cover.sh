5faf73cf54d8fb156fc0177bc55d842350f6a530
Guillaume Bailleul <gbailleul@eliocity.com>
2016-04-07 01:12:37 +0200

    Fixed coverage

--- test_cover.sh
+++ test_cover.sh
@@ -3,7 +3,7 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v /vendor/); do
+for d in $(find . -maxdepth 1 -type d); do
     if ls $d/*.go &> /dev/null; then
         go test -coverprofile=profile.out -covermode=atomic $d
         if [ -f profile.out ]; then
